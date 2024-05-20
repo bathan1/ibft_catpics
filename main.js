657c4ad9d96e41700056b799cdafa300988f2f20
nathan <side_nathan@hotmail.com>
2024-05-20 14:14:30 -0400

    Get statically served react app to work with simulation fetch call

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const startSimulation = require("./binding/lib/binding.js");
 const express = require("express");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, "webapp/dist")));
+
 app.post("/simulation", (req, res) => {
     const { numNodes, numFaulty, hash } = req.body;
     try {
         const log = startSimulation(numNodes, numFaulty, hash);
+        console.log("ran sim!");
         return res.status(200).json({ log });
     } catch (err) {
         return res.status(500).send(err.message);
