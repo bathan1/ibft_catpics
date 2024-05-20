9ad55c4fb1f29c1aa43e3db9a33f3991f373d5a6
nathan <side_nathan@hotmail.com>
2024-05-20 13:49:48 -0400

    Get node to bind to port env var

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const app = express();
 app.use(express.json());
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.post("/simulation", (req, res) => {
     const { numNodes, numFaulty, hash } = req.body;
