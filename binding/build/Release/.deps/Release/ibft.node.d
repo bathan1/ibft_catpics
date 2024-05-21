670bc080ddd50e7f15f4a437aa095ee087b80f7d
nathan <side_nathan@hotmail.com>
2024-05-21 15:53:02 -0400

    remove

--- binding/build/Release/.deps/Release/ibft.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/ibft.node := ln -f "Release/obj.target/ibft.node" "Release/ibft.node" 2>/dev/null || (rm -rf "Release/ibft.node" && cp -af "Release/obj.target/ibft.node" "Release/ibft.node")
--- binding/out/conanbuild.sh
+++
@@ -1,1 +0,0 @@
-. "/home/bathan1/dev/ibft_catpics/binding/out/conanbuildenv-debug-x86_64.sh"
